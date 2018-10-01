bb163bc54f62397644d8f8b871ae51263071e602
Spaghettificationalism <github@hintel.org>
2018-10-01 17:18:29 +0330

    [Updated] Implemented tags filtering.

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -83,3 +83,14 @@ exports.getStoreBySlug = async (req, res, next) => {
     if (!store) return next();
     res.render('store', { title: store.name, store });
 }
+
+exports.getStoresByTag = async (req, res) => {
+    const tag = req.params.tag;
+    const tagQuery = tag || { $exists: true, $ne: [] }
+
+    const tagsPromise = Store.getTagsList();
+    const storesPromise = Store.find({ tags: tagQuery });
+    const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+
+    res.render('tag', { tags, title: 'Tags', tag, stores });
+}

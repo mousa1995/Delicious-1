f8ad7311dc108a00e4216de1259a6787adade401
Spaghettificationalism <github@hintel.org>
2018-10-08 18:56:40 +0330

    [Added] Hearts & Displaying our hearted stores.

--- routes/index.js
+++ routes/index.js
@@ -51,6 +51,7 @@ router.post('/account/reset/:token',
     catchErrors(authController.update)
 );
 router.get('/map', storeController.mapPage);
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 
 /* 
     API
@@ -58,5 +59,6 @@ router.get('/map', storeController.mapPage);
 
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
+router.post('/api/stores/:id/heart', catchErrors(storeController.heartStore));
 
 module.exports = router;
